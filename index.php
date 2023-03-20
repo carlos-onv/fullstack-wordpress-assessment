c4e6c8d78e740297a8e3a977a3684149dfb325c9
Moacir Braga <moacir@fullstacklabs.co>
2023-03-20 16:51:35 -0300

    Minimum files for post and page

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php get_header(); ?>
-Hello Baby!
+<main id="page-container">
+    <ul>
+        <?php if ( have_posts() ) : ?>
+            <?php while ( have_posts() ) : ?>
+                <?php the_post(); ?>
+                <li>
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_title(); ?>
+                    </a>
+                </li>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </ul>
+</main>
 <?php get_footer(); ?>
---
+++ page.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<main id="page-container">
+	<article class="page-article">
+		<h1><?php the_title() ?></h1>
+		<?php the_content() ?>
+	</article>
+</main>
+<?php get_footer(); ?>
