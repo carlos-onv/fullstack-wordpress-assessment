11165d56fb4329db7d7f5f09972bc243b76aab0e
Carlos Nuñez <45440272+carlos-onv@users.noreply.github.com>
2024-11-25 20:57:52 -0500

    Added ACF

--- page.php
+++ page.php
@@ -7,6 +7,20 @@ get_header();
         get_template_part('loops/page-content');
         ?>
     </main>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <?php if (get_field('image')): ?>
+                    <img src="<?php the_field('image'); ?>" alt="">
+                <?php endif; ?>
+            </div>
+            <div class="col-6">
+                <?php if (get_field('content')): ?>
+                    <?php the_field('content'); ?>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
 
 <?php
 get_footer();
